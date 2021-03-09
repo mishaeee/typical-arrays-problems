fcc63392bef5dd8601598d74f8a1d6dae272cfe4
mishaeee <eliseevmp@gmail>
2021-03-10 00:56:24 +0300

    feat: implement min, max and av logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!Array.isArray(array) || array.length == 0) {
+        return 0;
+    }
+    array.sort((a, b) => a - b);
+    return array[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!Array.isArray(array) || array.length == 0) {
+        return 0;
+    }
+    array.sort((a, b) => a - b);
+    return array[array.length - 1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!Array.isArray(array) || array.length == 0) {
+        return 0;
+    }
+    let storage = 0;
+    for (let d of array) {
+        storage += d;
+    }
+    return storage / array.length;
 }
